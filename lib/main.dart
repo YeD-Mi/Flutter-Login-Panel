bc8621836402d2b38ec3238870f2f23ca6391380
Yunus Emre <dmrlyunusemre@gmail.com>
2022-11-05 19:23:55 +0300

    loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Neyim Var?',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,41 +1,50 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:login/views/loginpage.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Colors.deepPurple.shade700,
           title: Center(child: Text("Neyim Var?"))),
       body: Padding(
         padding: const EdgeInsets.only(
-            right: 25.0, left: 25.0, top: 40.0, bottom: 75.0),
+            right: 15.0, left: 15.0, top: 10.0, bottom: 40.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Image.asset(
               'assets/images/foto1.jpg',
-              height: 350,
+              height: size.height * .65,
             ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.deepPurple.shade700),
-                onPressed: () {
-                  print('Giriş Yap');
-                },
-                child: Text('Giriş Yap')),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.deepPurple.shade700),
-                onPressed: () {
-                  print('Kayıt ol');
-                },
-                child: Text('Kayıt Ol'))
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.deepPurple.shade700),
+                    onPressed: () {
+                      print('Giriş Yap');
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    child: Text('Giriş Yap')),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.deepPurple.shade700),
+                    onPressed: () {
+                      print('Kayıt ol');
+                    },
+                    child: Text('Kayıt Ol'))
+              ],
+            ),
           ],
         ),
       ),
