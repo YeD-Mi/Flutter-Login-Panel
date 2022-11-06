10a776781608432e79d0e008f65f45605731eb36
Yunus Emre <dmrlyunusemre@gmail.com>
2022-11-06 11:24:23 +0300

    loginpage-design

--- lib/views/loginpage.dart
+++ lib/views/loginpage.dart
@@ -11,8 +11,57 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.deepPurple.shade700,
+          title: Center(child: Text("Neyim Var?"))),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.deepPurple.shade700),
+                  borderRadius: BorderRadius.circular(15)),
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextField(
+                        controller: emailController,
+                        cursorColor: Colors.deepPurple.shade700,
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.mail),
+                            hintText: 'E-Posta Adresi'),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        controller: passwordController,
+                        cursorColor: Colors.deepPurple.shade700,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.key),
+                          hintText: 'Şifre',
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('Giriş Yap'))
+          ],
+        ),
+      ),
+    );
   }
 }
